73ea0af9208541cdb8e48908c76da8e30adaeacb
TrtleUnit <113350372+TrtleUnit@users.noreply.github.com>
2024-09-19 09:42:21 +0200

    keuzedelen changes
    The challenges page is now changed to the keuezedelen page. And is now filled with tons of information about them

---
+++ views/keuzedelen/keuzedelen.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <?php require '../views/templates/head.php' ?>
+</head>
+
+<body>
+    <?php require '../views/templates/menu.php' ?>
+
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+
+<table>
+  <tr>
+    <td><a href="basis_programmeren_games.php">Basis programmeren van games</a></td>
+    <td>Relevantie van het keuzedeel: <br>
+    Het keuzedeel biedt de beginnend beroepsbeoefenaar een breder perspectief op de arbeidsmarkt. <br> 
+    Het blijkt dat op mbo-niveau arbeidsplaatsen beschikbaar zijn binnen bedrijven (veelal binnen het MKB), waar het vervaardigen van games niet de corebusiness is, maar waar ontwikkelen en realiseren van games wel zijdelings onderdeel zijn van de bedrijfsvoering, bijvoorbeeld <br>
+    in de vorm van korte opdrachten/projecten, of als creatief onderdeel binnen een groter project. <br>
+    Denk daarbij aan games die gebouwd worden voor mobile devices en educatieve webapplicaties die serious games zijn. <br>
+    Er is vraag naar bijvoorbeeld Applicatie- en mediadevelopers die ook kennis hebben van het programmeren van games. <br>
+    Er is vraag naar dergelijke allround programmeurs. <br> <br>
+    Beschrijving van het keuzedeel: Dit keuzedeel betreft een verdieping of verbreding in de richting van het programmeren van eenvoudige games van allerlei aard.
+    De beginnend beroepsbeoefenaar werkt daarbij vanuit een game engine. <br>
+  </td>
+  </tr>
+  <tr>
+    <td><a href="digitale-vaardigheden.html">Digitale vaardigheden</a></td>
+    <td>Relevantie van het keuzedeel:<br>
+    Dit keuzedeel is een generiek keuzedeel. <br>
+    Het kan gekoppeld worden aan alle mbo kwalificatiedossiers waarin sprake is van het verzamelen, produceren en beheren van informatie, gegevens en content.<br>
+    Dit geldt niet alleen voor de typische kantoorberoepen, maar in de algemene zin voor die beroepen waar gebruik wordt gemaakt van digitale informatie, gegevens en content als input voor verdere verwerking. <br>
+    Vervolgens kan men in het specifieke profiel nog de digitale kennis en vaardigheden aanwijzen die specifiek zijn voor dat profiel. <br> <br>
+    Beschrijving van het keuzedeel: <br>
+    Centraal in het keuzedeel staan de kennis, vaardigheden en competenties die de mbo deelnemer nodig heeft bij de uitoefening van zijn of haar beroep ongeacht het softwarepakket, de vorm van verwerking en werkwijze. <br>
+    Wij hebben te maken met een informatiesamenleving waarin er een toevloed is aan digitale informatie en data, op een grote diversiteit aan devices en waarbij werknemers steeds meer inzicht moeten hebben in de koppeling tussen systemen, het op juiste wijze verspreiden van informatie en de verantwoording voor toekomstige juiste bewaring. <br>
+    Alle informatie en communicatie wordt immers verspreid door en vindt plaats met behulp van systemen. <br>
+    Dit betekent dat niet alleen kennis over het systeem aanwezig moet zijn, maar dat ook een keuze moet worden gemaakt voor een vorm waarin digitaal wordt gecommuniceerd en de wijze waarop de werknemer te werk gaat. <br>
+    Tot slot moet er inzicht zijn in hoe effectief men ICT in kan zetten bij zijn beroep. <br>
+    Er is een relatie met het keuzedeel CE Digitale vaardigheden basis. <br>
+    Het verschil is dat er in het CE Digitale vaardigheden gevorderd extra werkprocessen zijn aangewezen die het CE Digitale vaardigheden basis overstijgen. <br>
+    Wij adviseren de mbo deelnemers die het keuzedeel CE Digitale vaardigheden basis al hebben gevolgd, alleen de extra onderdelen te laten doen. <br>
+    Andere verschillen liggen met name in de complexiteit: <br>
+    in het keuzedeel CE Digitale vaardigheden gevorderd zal de beroepsbeoefenaar vaker strategieën bedenken en methodes toepassen. <br>
+    CE Digitale vaardigheden gevorderd is daarom een keuzedeel dat bij uitstek geschikt is voor de niveau 3 en 4 kwalificaties.
+</td>
+  </tr>
+  <tr>
+    <td><a href="fotografie-basis.html">Fotografie basis</a></td>
+    <td>Relevantie van het keuzedeel:<br>
+    Met dit verbredende keuzedeel versterkt de beginnend beroepsbeoefenaar zijn positie op de arbeidsmarkt aangezien hij voor de
+    opdrachtgever duidelijke, kwalitatief goede functionele foto's kan maken van eindproducten. <br>
+    Daarnaast kan hij een duidelijk beeld geven van de locatie ter ondersteuning van het inmeten en vastleggen van andere gegevens.<br> <br>
+    Beschrijving van het keuzedeel: <br>
+    Met dit keuzedeel verkrijgt de beginnend beroepsbeoefenaar basiskennis en vaardigheden fotografie. <br>
+    In het keuzedeel komen het klaarzetten en bedienen van fotoapparatuur, eenvoudig bewerken van foto's en opslag- en bestandsbeheer aan bod. <br>
+    De opnames zijn gericht op registeren en documenteren van eindproducten.
+    </td>
+  </tr>
+  <tr>
+    <td><a href="innovaties-niveau4.html">Inspelen op innovaties geschikt voor niveau 4</a></td>
+    <td>Relevantie van het keuzedeel: <br>
+    Voor veel branches geldt: stilstand is achteruitgang. <br>
+    Het keuzedeel ‘Inspelen op innovaties’ beoogt beroepsbeoefenaren beter in staat te stellen in te spelen op trend en ontwikkelingen in een (aanpalende) branche en is daarmee relevant voor de arbeidsmarkt. <br> <br>
+    Beschrijving van het keuzedeel: <br>
+    Het keuzedeel stelt de beginnend beroepsbeoefenaar beter in staat in te spelen op innovaties binnen de branche door te reflecteren op eigen beroepsmatig handelen in het kader van innovatie. <br>
+  </td>
+  </tr>
+  <tr>
+    <td><a href="ondernemend-gedrag.html">Ondernemend gedrag (geschikt voor niveau 3 en 4)</a></td>
+    <td>Relevantie van het keuzedeel: <br>
+    Ondernemend gedrag wordt steeds belangrijker geacht in meer en meer arbeidssituaties. <br>
+    Niet alleen voor mensen die (deels) als zelfstandige aan de slag gaan, maar ook voor werknemers in loondienst. <br>
+    Een ondernemende houding van werknemers draagt bij aan het innovatief vermogen van een organisatie. <br>
+    Het is tegenwoordig één van de belangrijkste selectiecriteria bij de invulling van vacatures. <br> 
+    Ook wijst onderzoek uit dat het een belangrijke succesfactor is voor het carrièreverloop van een werknemer. <br><br>
+    Beschrijving van het keuzedeel: <br>
+    Ondernemend gedrag wordt in dit keuzedeel ontwikkeld via drie werkprocessen: <br>
+    (1) Onderzoekt zichzelf, zijn (werk)omgeving en verbetermogelijkheden <br>
+    (2) Signaleert mogelijkheden voor verandering en innovatie <br>
+    (3) Neemt initiatieven in zijn werk <br>
+    Deze werkprocessen zijn ingekaderd en ingevuld, zodanig dat deze herken- en uitvoerbaar zijn voor deelnemers/werknemers op niveau 3 en 4.<br>
+</td>
+  </tr>
+  <tr>
+    <td><a href="orientatie.php">Oriëntatie</a></td>
+    <td>Relevantie van het keuzedeel: <br>
+    De CE Ondernemerschap wordt op dit moment bij verschillende sectoren en opleidingen uitgevoerd. <br>
+    Met name aan mbo-niveau 4 wordt de CE aangeboden. <br>
+    Voor niveau 2 en ook soms voor niveau 3 is de CE Ondernemerschap te hoog gegrepen: een ‘light’ versie
+    biedt uitkomst om deze deelnemers bekend te maken met het ondernemerschap. <br>
+    Het doel van dit keuzedeel is om ondernemerschapsonderwijs te bieden ter oriëntatie: de deelnemer heeft na het volgen van dit keuzedeel inzicht in het zelfstandig ondernemerschap en kan een beargumenteerde keuze maken voor het wel of niet maken van vervolgstappen richting het ondernemerschap. <br> <br>
+    Beschrijving van het keuzedeel: <br>
+    In het keuzedeel 'oriëntatie op ondernemerschap' wordt verdieping gegeven t.a.v. de onderwerpen: het oriënteren op het starten
+    van een onderneming, het oriënteren op het runnen van een onderneming en het besluiten of ondernemen past bij de student</td>
+  </tr>
+  <tr>
+    <td><a href="verdieping-software.html">Verdieping software</a></td>
+    <td>Relevantie van het keuzedeel: <br>
+    Voor de arbeidsmarkt is het van belang om je als beginnend beroepsbeoefenaar te kunnen onderscheiden. <br>
+    Hiermee maak je meer kans op een baan. Uit vacatures blijkt dat hebben van specialistische kennis rond software een pre is bij een functie. De
+    toegevoegde waarde van dit keuzedeel bestaat dan ook uit het al tijdens de opleiding specialiseren in software voor de
+    arbeidsmarkt waarin de beginnend beroepsbeoefenaar gaat werken. <br>
+    Binnen de wereld van de ICT is het behalen van certificaten van belang. <br>
+    Dit keuzedeel geeft handvatten om eventuele certificaten buiten dit keuzedeel te behalen. <br><br>
+    <p1>Beschrijving van het keuzedeel: <br></p1>
+    In dit keuzedeel verdiept de beginnend beroepsbeoefenaar zich in de software die van belang is voor zijn beroepsuitoefening en om zich te specialiseren. <br>
+    De beginnend beroepsbeoefenaar oriënteert zich op zijn (toekomstige) arbeidsmarkt</td>
+  </tr>
+</table>
+
+        <?php require '../views/templates/footer.php' ?>
+
+
+</body>
+
+</html>
